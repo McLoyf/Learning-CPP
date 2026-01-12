410be1f5f76711378efa3b240a4ad5aad10960c0
McLoyf <alex.ortiz242005@gmail.com>
2026-01-11 18:38:07 -0600

    Complete 4.9
    Add input_bool_val.cpp to 4.9 folder

--- Learn C++/Chapter_4/4.9/4.9.cpp
+++ Learn C++/Chapter_4/4.9/4.9.cpp
@@ -1,1 +1,19 @@
 // Boolean values
+
+#include <iostream>
+
+bool b {};  // default initialize to false
+bool b1 { !true };  // initialized with the value false
+
+int main()
+{
+    std::cout << b << '\n';
+    std::cout << !b << '\n';
+
+    std::cout << std::boolalpha;    // prints bools as true or false (std::noboolalpha is used to turn it back off)
+
+    std::cout << b << '\n';
+    std::cout << !b << '\n';
+
+    return 0;
+}
