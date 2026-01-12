3b1eeb12ffe260e446bfa8b9931f7a3364dd0760
McLoyf <alex.ortiz242005@gmail.com>
2026-01-11 19:44:46 -0600

    Complete 4.11

--- Learn C++/Chapter_4/4.11/4.11.cpp
+++ Learn C++/Chapter_4/4.11/4.11.cpp
@@ -1,1 +1,14 @@
 // Chars
+
+#include <iostream>
+
+int main()
+{
+    char ch { 97 };      // initialize with integer 97 (ASCII code 97 is 'a')
+    char ch1 { 'a' };   // initalize with code point for 'a' (ASCII code 97)
+
+    std::cout << ch << '\n';
+    std::cout << ch1 << '\n';
+
+    return 0;
+}
