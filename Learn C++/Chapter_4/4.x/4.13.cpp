eb9332dd9999b9f3c724875e53ed143ec9c57530
McLoyf <alex.ortiz242005@gmail.com>
2026-01-11 21:34:08 -0600

    Complete chapter 4
    Note: did not answer question 3, will do later

--- Learn C++/Chapter_4/4.x/4.13.cpp
+++ Learn C++/Chapter_4/4.x/4.13.cpp
@@ -1,1 +1,34 @@
 // Chapter 4 summary and quiz
+
+#include <iostream>
+
+double doMath(double x, double y, char z)
+{
+    if(static_cast<int>(z) == 42)
+        return x * y;
+    else if(static_cast<int>(z) == 43)
+        return x + y;
+    else if(static_cast<int>(z) == 45)
+        return x -y;
+    else if(static_cast<int>(z) == 47)
+        return x / y;
+}
+
+int main()
+{
+    std::cout << "Enter a double value: ";
+    double x;
+    std::cin >> x;
+
+    std::cout << "Enter a double value: ";
+    double y;
+    std::cin >> y;
+
+    std::cout << "Enter +, -, *, /: ";
+    char input;
+    std::cin >> input;
+
+    std::cout << x << " " << input << " " << y << " is " << doMath(x, y, input) << '\n';
+
+    return 0;
+}
