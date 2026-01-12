d3f1abc3c348db84ef87933b76e2eb7072c72c5e
McLoyf <alex.ortiz242005@gmail.com>
2026-01-12 15:55:56 -0600

    Complete 5.2

--- Learn C++/Chapter_5/5.2/5.2.cpp
+++ Learn C++/Chapter_5/5.2/5.2.cpp
@@ -1,1 +1,5 @@
 // Literals
+
+float f { 4.1f };               // use 'f' suffix so the literal is a float and matches variable type of float
+double d { 4.1 };               // change variable type to double so it matches the literal type double
+double avogadro { 6.02e23 };    // 6.02 x 10^23 is double literal in scientific notation
