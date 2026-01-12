cee31f1aea9b43345cbeea2ff82c2a5f7e151e33
McLoyf <alex.ortiz242005@gmail.com>
2026-01-12 15:41:50 -0600

    Complete 5.1

--- Learn C++/Chapter_5/5.1/5.1.cpp
+++ Learn C++/Chapter_5/5.1/5.1.cpp
@@ -1,1 +1,18 @@
 // Constant variables (named constants)
+
+#include <iostream>
+
+const double gravity { 9.8 };   // Cannot be changed (ofc)
+int const sidesInSquare { 4 };  // Also works but not preferred (this style of const declaration called "east const")
+
+void printGravity(double grav)
+{
+    std::cout << "gravity: " << grav << '\n';
+}
+
+int main()
+{
+    printGravity(3.71);
+
+    return 0;
+}
