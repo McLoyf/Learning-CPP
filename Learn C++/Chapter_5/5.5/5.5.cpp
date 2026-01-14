2cbc0c20d298713e227a50aad194a76b76bfe1e0
McLoyf <alex.ortiz242005@gmail.com>
2026-01-14 10:15:02 -0600

    Complete 5.5

--- Learn C++/Chapter_5/5.5/5.5.cpp
+++ Learn C++/Chapter_5/5.5/5.5.cpp
@@ -1,1 +1,5 @@
 // Constant expressions
+
+char a { 'q' };         // the value is constant but a isn't constant
+const int v { 0 };      // value is constant and v is constant
+const double c { 5.0 }; // value is constant but c isn't so this isn't a compile time constant
