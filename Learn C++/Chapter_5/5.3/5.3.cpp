d1e6929b74f5740f54121e077931ab6c9cf6f0b6
McLoyf <alex.ortiz242005@gmail.com>
2026-01-13 21:21:37 -0600

    Complete 5.3

--- Learn C++/Chapter_5/5.3/5.3.cpp
+++ Learn C++/Chapter_5/5.3/5.3.cpp
@@ -1,1 +1,8 @@
 // Numeral systems (decimal, binary, hexadecimal, and octal)
+
+main()
+{
+    int x { 012 };              // 0 before the number means this is an octal (but it's best to just avoid)
+    int y { 0xF };              // hexadecimal
+    int bin { 0b1011'0010 };    // assign binary 1011 0010 to the variable
+}
