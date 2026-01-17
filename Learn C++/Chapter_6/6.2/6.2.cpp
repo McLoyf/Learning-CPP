f2b6cff50402f89cb19550cce60c33dab77c289e
McLoyf <alex.ortiz242005@gmail.com>
2026-01-17 16:23:58 -0600

    Complete 6.2

--- Learn C++/Chapter_6/6.2/6.2.cpp
+++ Learn C++/Chapter_6/6.2/6.2.cpp
@@ -1,1 +1,28 @@
 // Arithmetic operators
+
+#include <iostream>
+
+int main()
+{
+    constexpr int x{ 7 };
+    constexpr int y{ 4 };
+
+    std::cout << "int / int = " << x / y << '\n';
+    std::cout << "double / int = " << static_cast<double>(x) / y << '\n';
+    std::cout << "int / double = " << x / static_cast<double>(y) << '\n';
+    std::cout << "double / double = " << static_cast<double>(x) / static_cast<double>(y) << '\n';
+
+    return 0;
+}
+
+/* Arithmetic assignment operators
+*  Addition assignment        Symbol: +=    Form: x += y    Operation: Add y to x
+*
+*  Subtraction assignment     Symbol: -=    Form: x -= y    Operation: Subtract y from x
+*
+*  Multiplication assignment  Symbol: *=    Form: x *= y    Operation: Multiply x by y
+*
+*  Division assignment        Symbol: /=    Form: x /= y    Operation: Divide x by y
+*
+*  Remainder assignment       Symbol: %=    Form: x %= y    Operation: Put the remainder of x / y in x
+*/
