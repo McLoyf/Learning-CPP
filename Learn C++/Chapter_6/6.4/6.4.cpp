2d39c1fedd5b9259dc7470700076e411efcee817
McLoyf <alex.ortiz242005@gmail.com>
2026-01-17 17:03:11 -0600

    Complete 6.4

--- Learn C++/Chapter_6/6.4/6.4.cpp
+++ Learn C++/Chapter_6/6.4/6.4.cpp
@@ -1,1 +1,29 @@
 // Increment/decrement operators, and side effects
+
+#include <iostream>
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+int main()
+{
+    int x { 5 };
+    int value { add(x, ++x) };  // undefined behavior: is this 5 + 6, or 6 + 6?
+                                // it depends on the order the compiler evaluates the function arguments in
+
+    std::cout << value << '\n'; // could be 11 or 12
+
+    return 0;
+}
+
+
+/* Incrementing and decrementing variables
+*
+*  Prefix increment     Symbol: ++    Form: ++x   Operation: Increment x, then return x
+*  Prefix decrement     Symbol: --    Form: --x   Operation: Decrement x, then return x
+*
+*  Suffix increment     Symbol: ++    Form: x++   Operation: Copy x, then increment x, then return the copy
+*  Suffic decrement     Symbol: --    Form: x--   Operation: Copy x, then decrement x, then return the copy
+*/
