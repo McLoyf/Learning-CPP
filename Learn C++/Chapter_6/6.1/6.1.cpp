a4bf1af798f17a9ac0ce9100aa20d7b4efed86de
McLoyf <alex.ortiz242005@gmail.com>
2026-01-17 16:10:43 -0600

    Complete 6.1

--- Learn C++/Chapter_6/6.1/6.1.cpp
+++ Learn C++/Chapter_6/6.1/6.1.cpp
@@ -1,1 +1,34 @@
 // Operator precedence and associativity
+
+#include <iostream>
+
+//#define AMBIG
+
+int getValue()
+{
+    std::cout << "Enter an integer: ";
+
+    int x;
+    std::cin >> x;
+    return x;
+}
+
+void printCalculation(int x, int y, int z)
+{
+    std::cout << x + (y * z);
+}
+
+int main()
+{
+    #ifdef AMBIG
+    printCalculation(getValue(), getValue(), getValue());   // This is ambiguous and may not produce intended results
+    #endif
+
+    int a{ getValue() };
+    int b{ getValue() };
+    int c{ getValue() };
+
+    printCalculation(a, b, c);  // this line is not unambigous
+
+    return 0;
+}
