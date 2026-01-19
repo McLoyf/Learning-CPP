8c0f98d7d1b794e6aae43b923bf86ae537911e0c
McLoyf <alex.ortiz242005@gmail.com>
2026-01-18 19:22:59 -0600

    Complete Chapter 6

--- Learn C++/Chapter_6/6.x/6.9.cpp
+++ Learn C++/Chapter_6/6.x/6.9.cpp
@@ -1,1 +1,39 @@
 // Chapter 6 summary and quiz
+
+#include <iostream>
+#include <string_view>
+
+std::string_view getQuantityPhrase(int x)
+{
+    if (x < 0)
+        return "negative";
+    else if (x == 0)
+        return "no";
+    else if (x == 1)
+        return "a single";
+    else if (x == 2)
+        return "a couple of";
+    else if (x == 3)
+        return "a few";
+    
+    return "many";
+}
+
+std::string_view getApplesPluralized(int x)
+{
+    return (x == 1) ? "apple" : "apples";
+}
+
+int main()
+{
+    constexpr int maryApples { 3 };
+    std::cout << "Mary has " << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
+
+    std::cout << "How many apples do you have? ";
+    int numApples{};
+    std::cin >> numApples;
+
+    std::cout << "You have " << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
+
+    return 0;
+}
