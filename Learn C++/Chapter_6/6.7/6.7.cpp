35029a4691367eb5f095982938c9830980e31447
McLoyf <alex.ortiz242005@gmail.com>
2026-01-18 18:52:05 -0600

    Complete 6.7
    Did not read through advanced reading, might come back to it later

--- Learn C++/Chapter_6/6.7/6.7.cpp
+++ Learn C++/Chapter_6/6.7/6.7.cpp
@@ -1,1 +1,24 @@
 // Relational operators and floating point comparisons
+
+#include <iostream>
+
+int main()
+{
+    constexpr double d1 { 100.0 - 99.99 };  // should equal 0.01
+    constexpr double d2 { 10.0 - 9.99 };    // should equal 0.01
+
+    if (d1 == d2)
+        std::cout << "d1 == d2" << '\n';
+    else if ( d1 > d2 )
+        std::cout << "d1 > d2" << '\n';
+    else if ( d1 < d2 )
+        std::cout << "d1 < d2" << '\n';
+
+    return 0;
+}
+
+/* d1 > d2 is printed
+*  
+*  d1 = 0.010000000000005116
+*  d2 = 0.0099999999999997868
+*/
