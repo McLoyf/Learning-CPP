b70cd957ccfa473b0339632ae9ca456d889fbfba
McLoyf <alex.ortiz242005@gmail.com>
2026-01-23 20:51:49 -0600

    Complete 7.1

--- Learn C++/Chapter_7/7.1/7.1.cpp
+++ Learn C++/Chapter_7/7.1/7.1.cpp
@@ -1,1 +1,23 @@
 // Compound statements (blocks)
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int value;
+    std::cin >> value;
+
+    if (value >= 0)
+    {
+        std::cout << value << " is a positive integer (or zero)\n";
+        std::cout << "Double this number is " << value * 2 << '\n';
+    }
+    else
+    {
+        std::cout << value << " is a negative integer\n";
+        std::cout << "The absolute value of this number is " << -value << '\n';
+    }
+
+    return 0;
+}
