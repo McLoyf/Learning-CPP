e27107a05d963e7535a44f6c848790e8ea7f66d6
McLoyf <alex.ortiz242005@gmail.com>
2026-03-04 18:34:57 -0600

    Complete 7.4

--- Learn C++/Chapter_7/7.4/7.4.cpp
+++ Learn C++/Chapter_7/7.4/7.4.cpp
@@ -1,1 +1,27 @@
 // Introduction to global variables
+
+#include <iostream>
+
+int g_x;        // variables declared out here are global and can be called from anywhere
+
+void doSomething()
+{
+    g_x = 3;
+    std::cout << g_x << '\n';
+}
+
+int main()
+{
+    doSomething();
+    std::cout << g_x << '\n';
+
+    g_x = 5;
+    std::cout << g_x << '\n';
+
+    return 0;
+}
+// g_x goes out of scope here
+
+/* Global variables are zero-initialized by default so even though the way I initialized it would normally result in
+   the value of g_x to be essentially random, because it's in the global scope it instead holds the value 0
+*/
